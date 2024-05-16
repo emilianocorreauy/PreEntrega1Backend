2c39b2d54bf66af5164ec6727e8a114957b04e14
emilianocorreauy <emiliano.correa9@gmail.com>
2024-05-15 23:28:21 -0300

    agregando app.use

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ const cartsRouter = require('./src/routes/carts.router');
 const app = express();
 const PORT = 8080;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// Middleware para parsear JSON
+app.use(express.json());
 
+// Middleware para parsear datos codificados en URL
+app.use(express.urlencoded({ extended: true }));
 
+// Definición de las rutas
 app.use('/', productsRouter);
 app.use('/', cartsRouter);
-
-
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
